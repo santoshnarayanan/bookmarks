e5afbf6fed9c6faf27aef227a74f0b861877521b
santosh <santosh33@gmail.com>
2024-06-14 23:26:31 +0200

    Allowing to edit profile

--- account/forms.py
+++ account/forms.py
@@ -2,6 +2,7 @@
 
 from django import forms
 from django.contrib.auth import get_user_model
+from .models import Profile
 
 
 class LoginForm(forms.Form):
@@ -22,3 +23,15 @@ class UserRegistrationForm(forms.ModelForm):
         if cd['password'] != cd['password2']:
             raise forms.ValidationError('Passwords dont match')
         return cd['password2']
+
+
+class UserEditForm(forms.ModelForm):
+    class Meta:
+        model = get_user_model()
+        fields = ['first_name', 'last_name', 'email']
+
+
+class ProfileEditForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['date_of_birth', 'photo']
