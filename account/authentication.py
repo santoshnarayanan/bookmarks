94184b2f813b83405e3784ef929d55dc485de30e
Santosh Narayanan <santosh33@gmail.com>
2024-06-18 23:52:40 +0200

    Create user profile that register with social authentication

--- account/authentication.py
+++ account/authentication.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import User
-
+from account.models import Profile
 class EmailAuthBackend:
     """
     Authenticate users using email.
@@ -18,3 +18,10 @@ class EmailAuthBackend:
             return User.objects.get(pk=user_id)
         except User.DoesNotExist:
             return None
+
+
+def create_profile(backend, user, *args, **kwargs):
+    """
+    Create a user profile for social authentication.
+    """
+    Profile.objects.get_or_create(user=user)
