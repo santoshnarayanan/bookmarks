0f4c84fea9a0d1a215d23f881d9c956f66003888
Santosh Narayanan <santosh33@gmail.com>
2024-06-21 00:09:37 +0200

    save image

--- images/forms.py
+++ images/forms.py
@@ -1,4 +1,8 @@
+import requests
 from django import forms
+from django.core.files.base import ContentFile
+from django.utils.text import slugify
+
 from .models import Image
 
 # Posting contents from other websites
@@ -22,7 +26,21 @@ class ImageCreateForm(forms.ModelForm):
         url = self.cleaned_data['url']
         valid_extensions = ['jpg', 'jpeg', 'png', 'gif']
         # The URL is split to check whether the file has a valid extension
-        extension = url.rsplit('.', 1)[1].lower()
-        if extension not in valid_extensions:
+        image_extension = url.rsplit('.', 1)[1].lower()
+        if image_extension not in valid_extensions:
             raise forms.ValidationError("The given URL does not match valid image extensions")
         return url
+
+    def save(self, force_insert=False, force_update=False, commit=True):
+        image = super().save(commit=False)
+        image_url = self.cleaned_data['url']
+        name = slugify(image.title)
+        image_ext = image_url.rsplit('.', 1)[1].lower()
+        image_name = f'{name}.{image_ext}'
+        # download image from the given URL
+        response = requests.get(image_url)
+        image.image.save(image_name, ContentFile(response.content), save=False)
+
+        if commit:
+            image.save()
+        return image
