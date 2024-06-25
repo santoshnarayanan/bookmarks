6c6b373aac9eab9d16ae9127cd7b1eb295fd075c
Santosh Narayanan <santosh33@gmail.com>
2024-06-25 18:45:04 +0200

    Added Id parameter

--- images/views.py
+++ images/views.py
@@ -35,7 +35,7 @@ def image_create(request):
                   )
 
 
-def image_detail(request, slug):
+def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
     return render(request, 'images/image/detail.html',
                   {'section': 'images', 'image': image}
