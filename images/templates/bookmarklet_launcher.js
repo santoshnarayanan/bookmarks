1b1d083c531b8bfc5ef1fdaf9e81c28e355be7e4
Santosh Narayanan <santosh33@gmail.com>
2024-06-25 17:53:07 +0200

    Formatted

--- images/templates/bookmarklet_launcher.js
+++ images/templates/bookmarklet_launcher.js
@@ -1,12 +1,9 @@
 (function () {
-    if(!window.bookmarklet){
+    if (!window.bookmarklet) {
         bookmarklet_js = document.body.appendChild(document.createElement('script'));
-        // Using a random number, we prevent the browser from loading
-        // the file from the browser’s cache.
-        bookmarklet_js.src = "127.0.0.1:8080/static/js/bookmarklet.js?r="+Math.floor(Math.random()*9999999999999999);
+        bookmarklet_js.src = '//127.0.0.1:8080/static/js/bookmarklet.js?r=' + Math.floor(Math.random() * 9999999999999999);
         window.bookmarklet = true;
-    }
-    else{
+    } else {
         bookmarkletLaunch();
     }
 })();
